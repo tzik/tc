84f9a254af6022d68e8704bf6b047737603dba81
Taiju Tsuiki <tzik@users.noreply.github.com>
2020-11-16 10:33:20 +0900

    Weekly version bump

--- lib/apply_patches.sh
+++ lib/apply_patches.sh
@@ -1,6 +1,9 @@
 
 apply_patches() {
   local i
+  if [ ! -d "${base_dir}/etc/patches" ]; then
+    return 0
+  fi
   while read -d $'\0' i; do
     if ! patch -d "${src_dir}" -R -f -s -p1 --dry-run -i "$i" > /dev/null 2>&1; then
       echo "Applying $(basename "$i")"
