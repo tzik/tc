8732858009a63e58a0f43f8e0df08811eb4e6cbd
tzik <mail@tzik.jp>
2020-11-19 03:08:07 +0900

    Update PYTHONPATH in env.sh

--- env.sh
+++ env.sh
@@ -1,2 +1,10 @@
 tc_dir="$(realpath -L "$(dirname "${BASH_SOURCE:-$0}")")"
-PATH="${tc_dir}/out/bin:${PATH}"
+export PATH="${tc_dir}/out/bin:${PATH}"
+
+get_site_packages() {
+  local i
+  python -c $'import site\nfor s in site.getsitepackages(): print(s)' | \
+    head -n1 | \
+    sed -n 's:^.*/\([^/]*/site-packages\)$:\1:p'
+}
+export PYTHONPATH="${tc_dir}/out/lib/$(get_site_packages):${PYTHONPATH}"
