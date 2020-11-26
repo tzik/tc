73f32c650b85254ac1027448748540c2c7c9bbc9
tzik <mail@tzik.jp>
2020-11-27 05:51:54 +0900

    Add tc_on and tc_off for toolchain switch

--- env.sh
+++ env.sh
@@ -1,10 +1,34 @@
 tc_dir="$(realpath -L "$(dirname "${BASH_SOURCE:-$0}")")"
-export PATH="${tc_dir}/out/bin:${PATH}"
 
-get_site_packages() {
-  local i
-  python -c $'import site\nfor s in site.getsitepackages(): print(s)' | \
+declare -A tc_paths
+tc_paths[PATH]="${tc_dir}/out/bin"
+tc_paths[PYTHONPATH]="${tc_dir}/out/lib/$(python -c $'import site\nfor s in site.getsitepackages(): print(s)' | \
     head -n1 | \
-    sed -n 's:^.*/\([^/]*/site-packages\)$:\1:p'
+    sed -n 's:^.*/\([^/]*/site-packages\)$:\1:p')"
+
+tc_off() {
+  local k v
+  local IFS=":"
+  for k in "${(k)tc_paths[@]}"; do
+    local updated=()
+    while read -d":" v; do
+      if [ -n "${v}" -a "${v}" != "${tc_paths[$k]}" ]; then
+        updated+=("${v}")
+      fi
+    done < <(eval "echo \"\${$k}\"")
+    eval "$k=\"${updated[*]}\""
+  done
 }
-export PYTHONPATH="${tc_dir}/out/lib/$(get_site_packages):${PYTHONPATH}"
+
+tc_on() {
+  tc_off
+
+  local k
+  for k in "${(k)tc_paths[@]}"; do
+    local old="$(eval "echo \${$k}")"
+    if [ -n "${old}" ]; then
+      old=":${old}"
+    fi
+    eval "$k=\"${tc_paths[$k]}${old}\""
+  done
+}
